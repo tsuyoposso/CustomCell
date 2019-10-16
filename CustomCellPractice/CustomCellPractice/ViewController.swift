2f03df206ea23ba479c237cbb83275954d9abb2b
tsuyoposso <51411162+tsuyoposso@users.noreply.github.com>
2019-10-16 14:12:57 +0900

    first commit

---
+++ CustomCellPractice/CustomCellPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCellPractice
+//
+//  Created by 長坂豪士 on 2019/10/16.
+//  Copyright © 2019 NagaKe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
